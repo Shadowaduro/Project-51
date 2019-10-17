ec103843de768673b4e923d9fd7a6984f5b633fa
Shadowaduro <lizash03@hotmail.com>
2019-10-17 10:46:14 +0800

    New start
    Let's go boys

---
+++ Project 51/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public GameObject ketch;
+    public GameObject mech;
+    public Transform spawnLoc;
+    public string character;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        character = PlayerPrefs.GetString("selectedCharacter");
+
+        if (character == "Ketch")
+        {
+            ketch.SetActive(true);
+            ketch.transform.position = spawnLoc.position;
+           //GameObject C = Instantiate(ketch, spawnLoc.position, Quaternion.identity);
+           //C.name.Replace("(clone)", ketch.name);
+        }
+        else if (character == "Mech")
+        {
+            mech.SetActive(true);
+            mech.transform.position = spawnLoc.position;
+        }
+    }
+}
---
+++ Project 51/Assets/Scripts/Telporter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telporter : MonoBehaviour
+{
+    public GameObject player;
+    public Transform toPos;
+    public bool t;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1");
+        if (other.tag == "Player")
+        {
+            player = other.gameObject;
+            StartCoroutine(Teleporting());
+        }
+    }
+
+    IEnumerator Teleporting()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("2");
+        player.GetComponent<PlayerController>().enabled = false;
+        Debug.Log(player.transform.position);
+        player.transform.position = toPos.position;
+        t = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StopAllCoroutines();
+        Debug.Log("Canceled");
+    }
+
+    private void Update()
+    {
+        if (t == true)
+        {
+            if (player.transform.position == toPos.position)
+            {
+                player.GetComponent<PlayerController>().enabled = true;
+                Debug.Log(player.transform.position);
+                player = null;
+                t = false;
+            }
+        }
+    }
+}
